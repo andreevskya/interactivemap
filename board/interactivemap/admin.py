87f9cdc267158cf96eaeedcf3ef5c0de89defcfd
username <you@example.com>
2017-04-18 00:58:05 +0300

    [Feature] Admin interface 2

--- board/interactivemap/admin.py
+++ board/interactivemap/admin.py
@@ -1,14 +1,20 @@
 from django.contrib import admin
+from django.urls import reverse
 
 from .models import *
 
 class EmployeeAdmin(admin.ModelAdmin):
+	view_on_site = True
 	list_display = ('lastName', 'firstName', 'patronym', 'positions', 'room', 'visualizationBase')
 	list_display_links = ('lastName', 'firstName', 'patronym')
 	
 	def positions(self, obj):
 		return obj.get_posts()
 
+	def view_on_site(self, obj):
+		url = reverse("floor", kwargs={'number':obj.room.floor.number})
+		return url + '#employee-' + str(obj.id)
+		
 admin.site.register(Floor)
 admin.site.register(Room)
 admin.site.register(Division)
